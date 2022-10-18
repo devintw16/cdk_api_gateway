c0ead47936acd4616f6b1b976d7c51644e37487f
Devin Weeraratne <devintw16@gmail.com>
2022-10-19 00:15:03 +1100

    feat: Add basic API gateway resource

--- lib/cdk_api_gateway-stack.ts
+++ lib/cdk_api_gateway-stack.ts
@@ -1,13 +1,21 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class CdkApiGatewayStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     
-    const bucket = 's3-bucket' //test change to test push
+    const api = new apigateway.RestApi(this, 'books-api');
+
+    api.root.addMethod('ANY');
+
+    const books = api.root.addResource('books');
+    books.addMethod('GET');
+    books.addMethod('POST');
+
+    const book = books.addResource('{book_id}');
+    book.addMethod('GET');
+    book.addMethod('DELETE');
   }
 }
